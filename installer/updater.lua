165f150b5cdd113a5f98a12890bfb84af0563848
P1KaChU337 <121425454+P1KaChU337@users.noreply.github.com>
2025-09-10 13:41:29 +0500

    1.1 Build 1

--- installer/updater.lua
+++ installer/updater.lua
@@ -5,9 +5,10 @@
 local REPOSITORY  = "https://raw.githubusercontent.com/P1KaChU337/Reactor-Control-for-OpenComputers/refs/heads/main/"
 
 local filesToDownload = {
+  {url = REPOSITORY.."lib/event.lua",                path="/lib/event.lua"},
   {url = REPOSITORY.."home/obf_nr_v1.1.lua",                path="/home/main.lua"},
 }
-local newVer = "1.1"
+local newVer = "1.1.1"
 local oldVer = "1.0"
 local f = io.open("oldVersion.txt", "r")
 if f then
@@ -191,5 +192,6 @@ if not ok then
   term.clear()
   io.stderr:write("Installer crashed: "..tostring(err).."\n")
 end
+
 
 
